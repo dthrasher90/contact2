dc1512584ce1fa6bc3907815559aa0a1a8d7efc7
dthrasher90 <dthrasher90@GMAIL.COM>
2017-01-06 11:10:05 -0900

    update
    update

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,42 +1,58 @@
 
 
 
-var app = angular.module("contactlistApp", []);
+var app = angular.module("contactlistApp", ['ngRoute']);
 
+ app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+     .when('/', {
+       templateUrl :'pages/home.html',
+       controller : 'MainCtrl'
+    })
+     .when('/addcontact', {
+        templateUrl : 'pages/addcontact.html'
+      //   controller : 'addContactCtrl'
 
-app.controller("MainCtrl", function($scope, $http){
+      });
 
-var id = $scope.contact_id;
+      $routeProvider.otherwise({redirectTo: '/'});
+      $locationProvider.html5Mode({enabled: true, requireBase: false});
+});
 
-$http.get('/contactlist').then(function(response){
-  console.log(" i got the data requested.");
-  $scope.contacts = response.data; // remove id from scope array to
-  });
+app.controller("addContactCtrl", function($scope, $http, $rootScope){
+  console.log("inside addContactCtrl");
+});
+
+app.controller("MainCtrl", function($scope, $http, $rootScope){
+  var id = $scope.contact_id;
+
+    $http.get('/contactlist').then(function(response){
+    console.log(" i got the data requested.");
+    $scope.contacts = response.data; // remove id from scope array to
+        });
+
 
 $scope.setContact = function(contactid) {
-  $scope.selectId = contactid;
-  console.log($scope.selectId);
-
+      $rootScope.selectId = contactid;
+      console.log($scope.selectId);
 }
 
-$scope.createContact = function(){
-  console.log("the create button was pressed ");
-  console.log($scope.selectId);
-}
 
 $scope.editContact = function (){
   console.log("the edit button was pressed " + $scope.selectId);
 }
 
-$scope.deleteContact = function(){
-  $http.delete('/contactlist/' + $scope.selectId);
 
-  // called asynchronously if an error occurs
-  // or server returns response with an error status.
+      });
 
-//create refresh funciotn in success callbqack, to reload the list
+app.controller('actionCtrl', function($scope, $http, $rootScope) {
+  $scope.deleteContact = function(){
+    console.log("deleteContact");
 
-  console.log("the delete button was pressed" + $scope.selectId);
-}
+        $http.delete('/contactlist/' + $rootScope.selectId);
+        console.log("the delete button was pressed" + $rootScope.selectId);
+
+
+          }
 
 });
